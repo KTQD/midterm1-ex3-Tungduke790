97679ef92e7b837ca6740390becaab0571c6d61d
Tungduke790 <157783262+Tungduke790@users.noreply.github.com>
2024-03-14 08:29:37 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,28 @@
+import java.io.*;
+
 public class Main {
     public static void main(String[] args) {
+        String fileIn = "input.txt";
+        String fileOut = "output.txt";
 
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(fileIn));
+            StringBuilder content = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                content.append(line).append("\n");
+            }
+            reader.close();
 
-        Bài làm viết tại đây
+            String replacedWord = content.toString().replaceAll("Nha Trang", "Vũng Tàu");
 
+            BufferedWriter writer = new BufferedWriter(new FileWriter(fileOut));
+            writer.write(replacedWord);
+            writer.close();
+
+            System.out.println("OK");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
